70a89a73b3baaacd39609138a33f886d060354b8
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-29 18:47:21 -0500

    Trial2
    I´m finally able to access the Pool API from BTC.com, now lets get into gathering the data

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -2,10 +2,11 @@
 #Last edited date: 29/10/22
 #Purpose: Gather Pool distribution, Bitcoin price and Transaction fee´s for Bitcoin for research purposes
 import requests as rq
+from bs4 import BeautifulSoup
 
 
-urlPoolApi = 'https://${pool.api.btc.com}/${v1}/${GET /account/address/update}'
+urlPoolApi = 'https://pool.api.btc.com/v1/pool/share-history'
 #the url acceses BTC.com Pool API
 rPoolApi = rq.get(urlPoolApi)
 json_data = rPoolApi.json()
-print(json_data.text())
+print(json_data)
