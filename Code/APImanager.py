54fb7c97a5ee113d7687646d8af658b49ac14b69
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-29 16:09:35 -0500

    Initial update

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -1,3 +1,9 @@
 #Created by: Lars Daniel Johansson Nino, Data Science undegradaute student and economics Research Assistant at ITAM
 #Last edited date: 29/10/22
 #Purpose: Gather Pool distribution, Bitcoin price and Transaction fee´s for Bitcoin for research purposes
+import requests as rq
+url = 'pending'
+r = rq.get(url)
+json_data = r.json()
+for key, value in json_data.items()
+    print(key + ' ',value)
