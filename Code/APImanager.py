963af3c3c14271501fd7011aca533324a5a41648
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-29 16:03:37 -0500

    Uploading code and literature

---
+++ Code/APImanager.py
@@ -0,0 +1,3 @@
+#Created by: Lars Daniel Johansson Nino, Data Science undegradaute student and economics Research Assistant at ITAM
+#Last edited date: 29/10/22
+#Purpose: Gather Pool distribution, Bitcoin price and Transaction fee´s for Bitcoin for research purposes
