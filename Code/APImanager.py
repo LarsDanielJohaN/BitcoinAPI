31136d6936299ce1692a26a8881aeeb68a3abedb
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-30 08:19:24 -0600

    Update APImanager.py

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -2,17 +2,23 @@
 #Last edited date: 29/10/22
 #Purpose: Gather Pool distribution, Bitcoin price and Transaction fee´s for Bitcoin for research purposes
 import requests as rq
-from bs4 import BeautifulSoup
+import calendar as cd
+import time as tm
 
 def main():
     urlPoolApi = 'https://pool.api.btc.com/v1/pool/share-history'
     paramsPool = {"dimension":"1d","start_ts":1647838478,"count":20}
-    #the url acceses BTC.com Pool API
-    rPoolApi = rq.get(urlPoolApi,paramsPool)
-    json_data = rPoolApi.json()
-    print(json_data)
+
+    json_Pool = getJson(urlPoolApi,paramsPool)
+    print(json_Pool)
+
+#method that returns a generalized request from a desired API
+def getJson(url,params):
+    r = rq.get(url,params)
+    return r.json();
 
 
 
-    
+
+
 main()
