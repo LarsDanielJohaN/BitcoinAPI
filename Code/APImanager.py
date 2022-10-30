4c319cd55538dc50f391f25636a7cbbde7c3ab6c
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-30 08:06:42 -0600

    T3.Better

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -4,9 +4,15 @@
 import requests as rq
 from bs4 import BeautifulSoup
 
+def main():
+    urlPoolApi = 'https://pool.api.btc.com/v1/pool/share-history'
+    paramsPool = {"dimension":"1d","start_ts":1647838478,"count":20}
+    #the url acceses BTC.com Pool API
+    rPoolApi = rq.get(urlPoolApi,paramsPool)
+    json_data = rPoolApi.json()
+    print(json_data)
 
-urlPoolApi = 'https://pool.api.btc.com/v1/pool/share-history?dimension=1d?time_strap=yyyy MMM dd?count=10'
-#the url acceses BTC.com Pool API
-rPoolApi = rq.get(urlPoolApi)
-json_data = rPoolApi.json()
-print(json_data)
+
+
+    
+main()
