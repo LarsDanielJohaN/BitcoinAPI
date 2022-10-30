518e79f9881ac6a01f4925a66b7e7eea37eeab78
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-30 08:59:17 -0600

    Progress

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -4,13 +4,21 @@
 import requests as rq
 import calendar as cd
 import time as tm
+import pandas as pd
 
 def main():
+
+    """
+    Specifics of data gathering of user history data can be looked at:
+    https://docs.pool.btc.com/#/share?id=get-user-history-data
+    """
+
     urlPoolApi = 'https://pool.api.btc.com/v1/pool/share-history'
     paramsPool = {"dimension":"1d","start_ts":1647838478,"count":20}
 
-    json_Pool = getJson(urlPoolApi,paramsPool)
-    print(json_Pool)
+    dataFrame = pd.DataFrame(getJson(urlPoolApi,paramsPool))
+
+    print(dataFrame.head())
 
 #method that returns a generalized request from a desired API
 def getJson(url,params):
