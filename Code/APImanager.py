44e3a9d4ed076aa5033d395a0b75d958faa8f747
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-31 10:23:28 -0600

    Update APImanager.py

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -10,6 +10,11 @@ import datetime
 def main():
 
     """
+    Current progress. The program is able to gather data from BTC.com´s api on user history data
+    Currently working on gathering the specific things we require. 
+    """
+
+    """
     Specifics of data gathering of user history data are available at:
     https://docs.pool.btc.com/#/share?id=get-user-history-data
     """
