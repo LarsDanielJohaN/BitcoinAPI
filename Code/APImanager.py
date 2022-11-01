57393d6614be67d7845858ae7acabe9706b655f9
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-10-31 19:58:54 -0600

    FinalyWorks!

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -7,6 +7,12 @@ import time as tm
 import pandas as pd
 import datetime
 
+"""
+might be useful?
+https://binance-docs.github.io/apidocs/spot/en/#hashrate-resale-list-user_data
+"""
+
+
 def main():
     #sets initial start date as the 1st of july of 2013
     initDateTimeStamp = int((datetime.datetime(2003,7,1)).timestamp())
@@ -15,16 +21,19 @@ def main():
 
     #works
     urlT = 'https://api.blockchain.info/charts/hash-rate?timespan=5weeks&rollingAverage=8hours&format=json'
-    #hasnt worked yet
-    urlP = 'https://api.blockchain.info/charts/pools?timespan=5days'
-    temp = getJson(urlP)
+    #works
+    urlP = 'https://blockchain.info/q/hashrate'
+    #FINALY WORKS!!!
+    urlC = "https://api.blockchain.info/pools?timespan=5days"
+    temp = getJson(urlC)
 
-    #dataFrame = pd.DataFrame('data': getJson(urlP))
+    dataFrame = pd.DataFrame(getJson(urlC), index = [0])
 
-
-    #print(dataFrame.keys())
-    #print(dataFrame.head())
     print(temp)
+    print(temp['AntPool'])
+    print(dataFrame.keys())
+    print(dataFrame.head())
+    #print(temp)
 
 
 #method that returns a generalized request from a desired API
