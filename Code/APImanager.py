34eacdb958b9f69b463b0f986005a714a93d9968
Lars Daniel Johansson Niño <82372128+LarsDanielJohaN@users.noreply.github.com>
2022-11-01 18:09:20 -0600

    Update APImanager.py

--- Code/APImanager.py
+++ Code/APImanager.py
@@ -1,5 +1,5 @@
 #Created by: Lars Daniel Johansson Nino, Data Science undegradaute student and economics Research Assistant at ITAM
-#Last edited date: 29/10/22
+#Last edited date: 1/11/22
 #Purpose: Gather Pool distribution, Bitcoin price and Transaction fee´s for Bitcoin for research purposes
 import requests as rq
 import calendar as cd
